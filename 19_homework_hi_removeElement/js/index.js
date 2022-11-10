3d4ee8416fc1a54f6e465f553a0e55606a5c5d83
elenalynx <elnlynx@gmail.com>
2022-11-10 19:51:33 +0200

    added homework 19 (function removed element in array)

---
+++ 19_homework_hi_removeElement/js/index.js
@@ -0,0 +1,16 @@
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, number) {
+    array.forEach((value, index) => {
+        if (value === number) {
+            array.splice(index, 1);
+        }
+    });
+    return array;
+}
+
+removeElement(array, 5);
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
