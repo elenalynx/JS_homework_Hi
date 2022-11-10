913585240cad1a4ead7e343e6cf1f27118b95a85
elenalynx <elnlynx@gmail.com>
2022-11-11 01:19:51 +0200

    added console.log('_HW_');

--- 19_homework_hi_removeElement/js/index.js
+++ 19_homework_hi_removeElement/js/index.js
@@ -1,5 +1,8 @@
 //Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array
 
+console.log('______HW 19_______');
+
+
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, number) {
